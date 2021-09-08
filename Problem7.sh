8f6801c87b3b704970e16c6fd6090f5091483b31
DZBohan <781783051@qq.com>
2021-09-08 10:58:26 -0700

    Submitting

---
+++ Problem7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 filename."
+    exit 1
+fi
+ 
+url=$1
+
+wget -O data "$url" -q 
+
+echo "$datafile"
+
+while read -n 1 c
+do
+    echo "$c"
+done < "data" | grep '[[:alpha:]]' | sort | uniq -c | sort -nr
